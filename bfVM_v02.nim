0eeaad4abf3379f955af44ded247dbff6cfb7f30
mratsim <mamy_github@numforge.co>
2018-10-09 15:32:25 +0200

    initialize a v2 with a jump table

---
+++ bfVM_v02.nim
@@ -0,0 +1,121 @@
+# Brainfuck interpreter v2: storing jump targets ahead of time
+# This is actually 25% slower than v1
+
+import streams, tables
+
+when defined(vmTrace):
+  import strformat
+
+type
+  BfOpKind = enum
+    opINCP   # Increment pointer
+    opDECP   # Decrement pointer
+    opINCA   # Increment byte at pointer address
+    opDECA   # Increment byte at pointer address
+    opDUMP   # Print byte
+    opSTOR   # Store byte at pointer address
+    opJZ     # Jump if zero
+    opBNZ    # Backtrack if not zero
+    opHALT   # End of instructions
+
+  BrainfuckVM = object
+    code: seq[BfOpKind]         # Operations
+    pc: int                     # Program counter: Address of the current instruction
+    mem: seq[uint8]             # Memory
+    I: int                      # Memory address register
+    jmpTargets: Table[int, int] # Jump targets
+
+const MemSize = 30000   # Initial memory of the VM
+
+proc lexBrainFuck(
+        result: var seq[BfOpKind],
+        stream: Stream,
+        jmpTargets: var Table[int, int]
+        ) =
+  # We use the function call stack
+  var startLoopPos: int
+  var stack: seq[int]
+
+  while not stream.atEnd():
+    case stream.readChar()
+    of '>': result.add opINCP
+    of '<': result.add opDECP
+    of '+': result.add opINCA
+    of '-': result.add opDECA
+    of '.': result.add opDUMP
+    of ',': result.add opSTOR
+    of '[':
+      result.add opJZ
+      stack.add result.len
+    of ']':
+      result.add opBNZ
+      let startLoop = stack.pop()
+      let endLoop = result.len
+      jmpTargets.add startLoop, endLoop
+      jmpTargets.add endLoop, startLoop
+    else: discard
+
+proc initBrainfuckVM(stream: Stream): BrainfuckVM =
+  result.jmpTargets = initTable[int, int](initialSize = 16)
+  lexBrainFuck(result.code, stream, result.jmpTargets)
+  result.mem = newSeq[uint8](MemSize)
+
+func next(vm: var BrainfuckVM): BfOpKind {.inline.}=
+  if vm.pc == vm.code.len:
+    return opHALT
+  result = vm.code[vm.pc]
+  inc vm.pc
+
+when defined(vmTrace):
+  proc traceBefore(vm: BrainfuckVM) =
+    # Need to be separated, the if/else interferes with computed goto labels
+    if vm.pc < vm.code.len:
+      stdout.write &"\nBefore - pc: {vm.pc:>03}, op: {vm.code[vm.pc]:<6}, I: {vm.I:>04}, mem: {vm.mem[vm.I]:>03}"
+    else:
+      stdout.write "Before - No code left, end of execution"
+
+  proc traceAfter(vm: BrainfuckVM) =
+    # Need to be separated, the if/else interferes with computed goto labels
+    if vm.pc < vm.code.len:
+      stdout.write &"-   After - pc: {vm.pc:>03}, op: {vm.code[vm.pc]:<6}, I: {vm.I:>04}, mem: {vm.mem[vm.I]:>03}"
+    else:
+      stdout.write "-   After - No code left, end of execution"
+
+proc executeOpcodes(vm: var BrainfuckVM) =
+  when defined(vmTrace):
+    echo vm.code
+  while true:
+    {.computedGoto.}
+    when defined(vmTrace):
+      vm.traceBefore()
+    case vm.next():
+    of opINCP: inc vm.I
+    of opDECP: dec vm.I
+    of opINCA: inc vm.mem[vm.I]
+    of opDECA: dec vm.mem[vm.I]
+    of opDUMP:
+      stdout.write cast[char](vm.mem[vm.I])
+      flushFile(stdout)
+    of opSTOR: discard stdin.readBytes(vm.mem, vm.I, 1)
+    of opJZ:
+      if vm.mem[vm.I] == 0:
+        vm.pc = vm.jmpTargets[vm.pc]
+    of opBNZ:
+      if vm.mem[vm.I] != 0:
+        vm.pc = vm.jmpTargets[vm.pc]
+    of opHALT:
+      break
+    when defined(vmTrace):
+      vm.traceAfter()
+
+proc execBFfile*(file: string) =
+  let s = openFileStream(file)
+  defer: s.close()
+  var vm = s.initBrainfuckVM()
+  vm.executeOpcodes()
+
+proc execBFstring*(prog: string) =
+  let s = newStringStream(prog)
+  defer: s.close()
+  var vm = s.initBrainfuckVM()
+  vm.executeOpcodes()
