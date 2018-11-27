cf5781e2728401d74a948dec1010d4a6be26ce68
mratsim <mamy_github@numforge.co>
2018-11-27 19:50:17 +0100

    improved rex prefix for scalar arithmetic

--- jit/jit_x86_64_scalar_arith.nim
+++ jit/jit_x86_64_scalar_arith.nim
@@ -8,42 +8,38 @@ func inc*(a: var Assembler[Reg_X86_64], reg: static Reg_X86_64) {.inline.} =
   ## Increment a register by 1
   ## Note that the Carry Flag is not updated
   ## in case of rollover
-  const is_low_reg = reg in rax..rdi
   a.code.add [
-    rex_prefix(w = 1),
+    rex_prefix(w = 1, b = int(reg in r8..r15)),
     0xFF,
-    modrm(Direct, opcode_extension = 0, rm = reg, not is_low_reg)
+    modrm(Direct, opcode_extension = 0, rm = reg)
   ]
 
 func dec*(a: var Assembler[Reg_X86_64], reg: static Reg_X86_64) {.inline.} =
   ## Decrement a register by 1
   ## Note that the Carry Flag is not updated
   ## in case of rollover
-  const is_low_reg = reg in rax..rdi
   a.code.add [
-    rex_prefix(w = 1),
+    rex_prefix(w = 1, b = int(reg in r8..r15)),
     0xFF,
-    modrm(Direct, opcode_extension = 1, rm = reg, not is_low_reg)
+    modrm(Direct, opcode_extension = 1, rm = reg)
   ]
 
 func inc*(a: var Assembler[Reg_X86_64], adr_reg: static array[1, Reg_X86_64]) {.inline.} =
   ## Increment the memory location pointed to by the register by 1
   ## Note that the Carry Flag is not updated
   ## in case of rollover
-  const is_low_reg = adr_reg[0] in rax..rdi
   a.code.add [
-    rex_prefix(w = 1),
+    rex_prefix(w = 1, b = int(adr_reg[0] in r8..r15)),
     0xFF,
-    modrm(Indirect, opcode_extension = 0, rm = adr_reg[0], not is_low_reg)
+    modrm(Indirect, opcode_extension = 0, rm = adr_reg[0])
   ]
 
 func dec*(a: var Assembler[Reg_X86_64], adr_reg: static array[1, Reg_X86_64]) {.inline.} =
   ## Decrement the memory location pointed to by the register by 1
   ## Note that the Carry Flag is not updated
   ## in case of rollover
-  const is_low_reg = adr_reg[0] in rax..rdi
   a.code.add [
-    rex_prefix(w = 1),
+    rex_prefix(w = 1, b = int(adr_reg[0] in r8..r15)),
     0xFF,
-    modrm(Indirect, opcode_extension = 1, rm = adr_reg[0], not is_low_reg)
+    modrm(Indirect, opcode_extension = 1, rm = adr_reg[0])
   ]
